a73797119b8d7b4bd811fa7d4c30e143d65fc7a1
Osama Soliman <eng.microsmsm@gmail.com>
2019-11-13 15:36:58 +0200

    adding search posibilites

--- lib/pages/drug.dart
+++ lib/pages/drug.dart
@@ -6,16 +6,31 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:diff_match_patch/diff_match_patch.dart';
 
-
 class _DrugPageState extends State<DrugPage> {
   List<Drug> drugs;
-  StreamController drugController = new StreamController<List<Drug>>.broadcast();
+  StreamController drugController =
+      new StreamController<List<Drug>>.broadcast();
   String term = "";
+  String searchBy = "tradename";
+  final objectSchema = {
+    "id":"ID",
+    "tradename": "Trade Name",
+    "activeingredient": "Active Ingredent",
+    "price": "Price",
+    "company": "Company",
+    "group": "Group",
+    "pamphlet":"Pamphlet",
+    "dosage":"Dose",
+    "composition":"Composition"
+  };
+
+
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    fetchDrugs().then((data){
+    fetchDrugs().then((data) {
       drugs = data;
       drugController.add(data);
     });
@@ -38,13 +53,71 @@ class _DrugPageState extends State<DrugPage> {
     );
   }
 
-
-
-
-
-  onTextChanged(text){
-    doSearch(drugs, text, drugController);
+  onTextChanged(text) {
+    doSearch(drugs, text,searchBy, drugController);
     term = text;
+  }
+
+
+  final objectArraySchema = [
+    {"key": "id", "value": "ID"},
+    {"key": "tradename", "value": "Trade Name"},
+    {"key": "activeingredient", "value": "Active Ingredent"},
+    {"key": "price", "value": "Price"},
+    {"key": "company", "value": "Company"},
+    {"key": "group", "value": "Group"},
+    {"key": "pamphlet", "value": "Pamphlet"},
+    {"key": "dosage", "value": "Dose"},
+    {"key": "composition", "value": "Composition"}
+  ];
+
+  Future<String> filterDialog() async {
+
+      switch (await showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return SimpleDialog(
+              title: const Text('Search By'),
+              children: <Widget>[
+
+            for (var object in objectArraySchema) SimpleDialogOption(
+                  onPressed: () {
+                    Navigator.pop(context, object["key"]);
+                  },
+                  child: Text(object["value"]),
+                ),
+              ],
+            );
+          })) {
+        case "id":
+          return "id";
+          break;
+        case "tradename":
+          return "tradenmae";
+          break;
+        case "activeingredient":
+          return "activeingredient";
+          break;
+        case "price":
+          return "price";
+          break;
+        case "company":
+          return "company";
+          break;
+        case "group":
+          return "group";
+          break;
+        case "pamphlet":
+          return "pamphlet";
+          break;
+        case "dosage":
+          return "dosage";
+          break;
+        case "composition":
+          return "composition";
+          break;
+      }
+
   }
 
   Widget searchCard() {
@@ -57,14 +130,25 @@ class _DrugPageState extends State<DrugPage> {
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
-              Icon(Icons.search),
+              GestureDetector(
+                child: Icon(Icons.search),
+                onTap: () {
+                  //open options
+                  filterDialog().then((res) {
+                    setState(() {
+                      searchBy = objectSchema[res];
+                    });
+                  });
+                },
+              ),
               SizedBox(
                 width: 10.0,
               ),
               Expanded(
                 child: TextField(
                   decoration: InputDecoration(
-                      border: InputBorder.none, hintText: "Search drug by tradename..."),
+                      border: InputBorder.none,
+                      hintText: "Search by " + searchBy +" ..."),
                   onChanged: onTextChanged,
                 ),
               ),
@@ -76,7 +160,6 @@ class _DrugPageState extends State<DrugPage> {
     );
   }
 
-
   Widget drugStreamList() {
     return new StreamBuilder<List<Drug>>(
       stream: getStream(),
@@ -86,10 +169,13 @@ class _DrugPageState extends State<DrugPage> {
           case ConnectionState.none:
             return Text('Status: none');
           case ConnectionState.waiting:
-            return Text('Loading... ⏳',textAlign: TextAlign.center,);
+            return Text(
+              'Loading... ⏳',
+              textAlign: TextAlign.center,
+            );
           case ConnectionState.active:
             return drugListView(context, snapshot);
-        //add data here
+          //add data here
           case ConnectionState.done:
             return drugListView(context, snapshot);
         }
@@ -97,38 +183,46 @@ class _DrugPageState extends State<DrugPage> {
       },
     );
   }
-  Widget drugListView(BuildContext context, AsyncSnapshot snapshot) {
-        if(term.length == 0){
-          return Text('Ready to Search 😁', textAlign: TextAlign.center,);
-        }else{
-          List<Drug> values = snapshot.data;
-          return  ListView.builder(
-            physics: const AlwaysScrollableScrollPhysics(),
-            shrinkWrap: true,
-            itemCount: values.length,
-            itemBuilder: (BuildContext context, int index) {
-            return drugCard(values[index]);
-            });
-        }
 
+  Widget drugListView(BuildContext context, AsyncSnapshot snapshot) {
+    if (term.length == 0) {
+      return Text(
+        'Ready to Search 😁',
+        textAlign: TextAlign.center,
+      );
+    } else {
+      List<Drug> values = snapshot.data;
+      return ListView.builder(
+          physics: const AlwaysScrollableScrollPhysics(),
+          shrinkWrap: true,
+          itemCount: values.length,
+          itemBuilder: (BuildContext context, int index) {
+            return drugCard(values[index]);
+          });
+    }
   }
+
   Widget drugCard(Drug drug) {
     return new Column(
       children: <Widget>[
         new ListTile(
-            leading: Icon(Icons.healing),
-            title: Text(drug.tradename,overflow: TextOverflow.ellipsis,),
-            subtitle: drug.activeingredient.length != null
-                ? Text(drug.activeingredient,overflow: TextOverflow.ellipsis)
-                : null
-        ,onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => DrugDetailsPage(drug: drug, drugs:drugs),
-            ),
-          );
-        },),
+          leading: Icon(Icons.healing),
+          title: Text(
+            drug.tradename,
+            overflow: TextOverflow.ellipsis,
+          ),
+          subtitle: drug.activeingredient.length != null
+              ? Text(drug.activeingredient, overflow: TextOverflow.ellipsis)
+              : null,
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => DrugDetailsPage(drug: drug, drugs: drugs),
+              ),
+            );
+          },
+        ),
         new Divider(
           height: 2.0,
         ),
